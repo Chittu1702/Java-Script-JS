856c2f5f9588f2a878202aaf83e037a95008abeb
Chittu1702 <112084202+Chittu1702@users.noreply.github.com>
2024-06-04 14:25:56 +0000

    conversion of datatype

---
+++ 01_basics/01_convertOperat.js
@@ -0,0 +1,13 @@
+let num = 33;
+let string = String(num);
+console.log(typeof string);
+
+let value = 1;
+let bool = Boolean(value);
+console.log(bool);
+console.log(typeof bool);
+
+let a  = undefined;
+let number = Number(a);
+console.log(number);
+console.log(typeof number);
